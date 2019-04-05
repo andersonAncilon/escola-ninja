2403f2e738200303b6966262b97caa886f06c45d
andersonAncilon <dan100anderson@gmail.com>
2019-04-04 23:27:59 -0300

    Working on home page, done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
 <template>
-  <v-container grid-list-md text-xs-center class="mb-5">
+  <v-container grid-list-md text-xs-center>
     <v-layout align-center justify-center fill-height>
-      <v-card color="blue-grey darken-2" class="white--text" width="45%" height="auto">
+      <v-card class="black--text" width="80vh" height="auto">
         <v-card-title primary-title>
-          <div>
-            <div class="headline">Unlimited music now</div>
-            <span>Listen to your favorite artists and albums whenever and wherever, online and offline.</span>
-          </div>
+          <v-layout column justify-center align-center>
+            <h2 class="display-2 mb-3">Bem vindo, você é:</h2>
+            <v-btn color="teal" class="white--text home-btn-size">Sou Aluno</v-btn>
+            <v-btn color="info" class="home-btn-size">Sou Professor</v-btn>
+            <v-btn outline color="error" class="home-btn-size">Ainda não tenho cadastro :(</v-btn>
+          </v-layout>
         </v-card-title>
         <v-card-actions>
           <v-btn flat dark>Listen now</v-btn>
@@ -20,5 +22,10 @@
 export default {};
 </script>
 
-<style scoped>
+<style lang="scss">
+
+.home-btn-size {
+  width: 75% !important
+}
+
 </style>
